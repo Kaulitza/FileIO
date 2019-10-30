7c90f7e9cb485abc5bdf2bd21c7c99b43ccba96b
Michelle Vanwettere <michellevanwettere@msn.com>
2019-10-30 10:37:35 -0400

    Add project files.

---
+++ FileIO/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string dataPath = "Data\\Data.txt";
+            string dataPath = @"Data\Data.txt";
+
+            string newLine = "\n";
+
+            string students;
+
+            string student1 = "Bonzo";
+            string student2 = "Lucy";
+
+            File.WriteAllText(dataPath, student1 + newLine);
+            File.AppendAllText(dataPath, student2);
+
+            students = File.ReadAllText(dataPath);
+        }
+    }
+}
